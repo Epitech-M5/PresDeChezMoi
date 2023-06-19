8a5ffa924edca65976a3a86a070869de3df7b1f5
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-06-19 17:15:10 +0200

    setting css

--- front/src/pages/LandingPage.js
+++ front/src/pages/LandingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBarLandingPage from '../components/NavBarLandingPage';
 
 const LandingPage = () => {
     return (
         <>
+            <NavBarLandingPage />
             <h1>landing page</h1>
         </>
     );
