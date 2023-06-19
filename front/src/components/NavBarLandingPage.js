d3d66379f56adc44c404953087e6053829cb064b
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-06-19 17:04:37 +0200

    archi 2 front

---
+++ front/src/components/NavBarLandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBarLandingPage = () => {
+    return (
+        <>
+            <h1>coucou je suis le navbar</h1>
+        </>
+    );
+};
+
+export default NavBarLandingPage;
