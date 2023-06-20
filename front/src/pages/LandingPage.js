0393452221fb3d6ddd3ea30f7fabc218ce8bea7d
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-06-20 10:44:24 +0200

    landing page button component and animation

--- front/src/pages/LandingPage.js
+++ front/src/pages/LandingPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import NavBarLandingPage from '../components/NavBarLandingPage';
+import ButtonLandingPage from '../components/ButtonLandingPage';
 
 const LandingPage = () => {
     return (
         <>
 
-            <div className='test'>
-                <NavBarLandingPage />
+
+            <div className="title">
+                <div className="main_title">
+                    <h1>PresDeChezMoi</h1>
+                </div>
+                <div className="main_title_2">
+                    <h3>Le réseau social de ta commune !</h3>
+                </div>
+                <div className="btn_title">
+                    <ButtonLandingPage text="C'est parti !" nagifation="/login" />
+                </div>
+            </div>
+            <div className="main_area">
+                <div className="circles">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </>
     );
