19e3c4b6842f45d583dc0b404f547acb7c3ce6dc
Morgan-gh <daraen@MSI>
2023-06-27 16:20:01 +0200

    userMenu incoming

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import Test from './pages/Test';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/test' element={<Test />} />
       </Routes>
     </BrowserRouter>
---
+++ front/src/pages/Home.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+import UserMenu from '../components/UserMenu';
+
+
+const Home = () => {
+
+    return (
+        <>
+            <h1>Home</h1>
+            <UserMenu />
+        </>
+    );
+};
+
+export default Home;
