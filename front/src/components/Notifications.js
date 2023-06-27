537371d5a4e7408eb4eb9006bc135ddbf0a436b2
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-06-27 16:07:40 +0200

    save

---
+++ front/src/components/Notifications.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+
+const Notifications = (props) => {
+
+    const [notif, setNotif] = useState();
+
+    useEffect(() => {
+
+        switch (props.type) {
+            case '1':
+                return setNotif(
+                    <li className="toast success">
+                        <div className="column">
+                            <i class="fa-solid fa-circle-check"></i>
+                            <span>{props.text}</span>
+                        </div>
+                        <i class="fa-solid fa-xmark"></i>
+                    </li>
+                );
+            case '2':
+                return setNotif(
+                    <li className="toast info">
+                        <div className="column">
+                            <i class="fa-solid fa-circle-info"></i>
+                            <span>{props.text}</span>
+                        </div>
+                        <i class="fa-solid fa-xmark"></i>
+                    </li>
+                );
+            case '3':
+                return setNotif(
+                    <li className="toast warning">
+                        <div className="column">
+                            <i class="fa-sharp fa-solid fa-circle-exclamation"></i>
+                            <span>{props.text}</span>
+                        </div>
+                        <i class="fa-solid fa-xmark"></i>
+                    </li>
+                );
+            case '4':
+                return setNotif(
+                    <li className="toast danger">
+                        <div className="column">
+                            <i class="fa-sharp fa-solid fa-ban"></i>
+                            <span>{props.text}</span>
+                        </div>
+                        <i class="fa-solid fa-xmark"></i>
+                    </li>
+                );
+            default:
+                return null;
+
+        }
+
+    }, [])
+
+    return (
+        <>
+            <ul className='notifications'>
+                {notif}
+            </ul>
+        </>
+    );
+};
+
+export default Notifications;
--- front/src/pages/Test.js
+++ front/src/pages/Test.js
@@ -1,40 +1,65 @@
 import { React, useEffect, useState } from 'react';
 import Loader from '../components/Loader.js'
 import axios from 'axios';
+import Notifications from '../components/Notifications.js';
 
 const Test = () => {
 
     const [data, setData] = useState([]);
     const [success, setSuccess] = useState(false);
+    const [list, setList] = useState();
 
-    useEffect(() => {
+    const handleFakeRequest = () => {
+
         axios
             .get('https://my-json-server.typicode.com/typicode/demo/db')
             .then((response) => {
                 setData([response.data]);
                 setSuccess(true);
+
             })
             .catch((error) => {
                 console.log("error " + error);
                 setSuccess(false);
             })
-    })
 
+    }
+
+    /*
+        useEffect(() => {
+            axios
+                .get('https://my-json-server.typicode.com/typicode/demo/db')
+                .then((response) => {
+                    setData([response.data]);
+                    setSuccess(true);
+                })
+                .catch((error) => {
+                    console.log("error " + error);
+                    setSuccess(false);
+                })
+        })
+    */
     return (
         <>
-            {
-                success ? (
-                    data.map((fake_data) => (
-                        <div key={fake_data.posts[0].id} className='testdiv'>
-                            <p>{fake_data.posts[0].title}</p>
+            <div className="abs_test_container">
+                {
+                    success ? (
+                        data.map((fake_data) => (
+                            <div key={fake_data.posts[0].id} className='testdiv'>
+                                <p>{fake_data.posts[0].title}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <div className='testdiv'>
+                            <Loader />
                         </div>
-                    ))
-                ) : (
-                    <div className='testdiv'>
-                        <Loader />
-                    </div>
-                )
-            }
+                    )
+                }
+                <Notifications type='1' text='ddddddd d dddd ddd' />
+                <Notifications type='2' text='ddddddd d dddd ddd' />
+                <button onClick={handleFakeRequest}>Request</button>
+
+            </div>
         </>
     );
 };
