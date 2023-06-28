4abf40572b0447ab2bafe08b763f2773df5628d6
Morgan-gh <navarramorgan@hotmail.fr>
2023-06-29 00:30:05 +0200

    ajustement

--- front/src/components/UserMenu.js
+++ front/src/components/UserMenu.js
@@ -19,37 +19,37 @@ const Home = (props) => {
                 className="ms-menu-toggle"
                 name="ms-menu-toggle"
               />
-              <li className="ms-li ms-li1 ms-li-first">
-                <a href="javascript:void(0)">
-                  <span className="fa fa-fort-awesome"></span>
-                </a>
-              </li>
-              <li className="ms-li ms-li2">
-                <a href="javascript:void(0)">
-                  <span className="fa fa-flask"></span>
-                </a>
-              </li>
-              <li className="ms-li ms-li3 ms-li-last">
-                <a href="javascript:void(0)">
-                  <span className="fa fa-search"></span>
-                </a>
-              </li>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
-              <div className="ms-nav-point"> </div>
               <li className="ms-main">
                 <a href="javascript:void(0)">
                   <label className="ms-menu-toggle-lbl" for="ms-menu">
                     <img src={mainLogo} alt="logo" className="userProfile" />
                     <h3>Pseudo</h3>
                   </label>
+                </a>
+              </li>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <div className="ms-nav-point"> </div>
+              <li className="ms-li ms-li1 ms-li-first">
+                <a href="javascript:void(0)">
+                  <span className="fa fa-search"></span>
+                </a>
+              </li>
+              <li className="ms-li ms-li2">
+                <a href="javascript:void(0)">
+                  <span className="fa fa-cog"></span>
+                </a>
+              </li>
+              <li className="ms-li ms-li3 ms-li-last">
+                <a href="javascript:void(0)">
+                  <span className="fa fa-sliders"></span>
                 </a>
               </li>
             </ul>
