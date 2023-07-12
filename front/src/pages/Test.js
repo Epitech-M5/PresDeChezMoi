a4713e7e8a7d7c13a2e3864f26dd2638101fb4f9
mxe <110362553+ImMaxence@users.noreply.github.com>
2023-07-12 13:33:08 +0200

    fix bug navbar landing page + dropdown btn done

--- front/src/pages/Test.js
+++ front/src/pages/Test.js
@@ -1,37 +1,23 @@
-import { React, useEffect } from 'react';
-import useAxios from '../api';
-import { getAPI, postAPI, putAPI, deleteAPI } from '../api';
+import { React, useState } from 'react';
+import DropDownBtn from '../components/MainComponent/DropDownBtn';
 
 
 const Test = () => {
 
-    // useEffect(() => {
-    //     get("http://127.0.0.1:8081/api/status/")
-    // }, []);
+    const [selectedValue, setSelectedValue] = useState(null);
 
-    // const { dataAPI, isLoadingAPI, errorAPI, get, post, put, del } = useAxios();
-
-    const handleRRR = () => {
-        // console.log(dataAPI, isLoadingAPI, errorAPI)
-        const url = 'http://127.0.0.1:8081/api/status/2'; // Remplacez par l'URL de l'API que vous souhaitez interroger
-
-        getAPI(url, null, null).then((response) => {
-            console.log('Réponse de l\'API :', response);
-            // Faites quelque chose avec la réponse de l'API ici
-        }).catch((error) => {
-            console.error('Erreur lors de l\'appel à l\'API :', error);
-        });
-        // const rrrr = get("http://127.0.0.1:8081/api/status/")
-
-        // console.log("data : " + rrrr)
-        // , isLoadingAPI, errorAPI, dataAPI)
-    }
+    const handleCheckboxChange = (item) => {
+        setSelectedValue(item);
+        console.log('Selected value in parent component: ' + item);
+    };
 
     return (
         <div className='center'>
-
-            <button onClick={handleRRR}>request</button>
-
+            <DropDownBtn text="Type de post" items={['Vente', 'Evénement', 'Poste à pourvoir', 'Promotion']} onCheckboxChange={handleCheckboxChange} />
+            <DropDownBtn text="Type de post" items={['Vente', 'Evénement', 'Poste à pourvoir', 'Promotion']} onCheckboxChange={handleCheckboxChange} />
+            <DropDownBtn text="Type de post" items={['Vente', 'Evénement', 'Poste à pourvoir', 'Promotion']} onCheckboxChange={handleCheckboxChange} />
+            <DropDownBtn text="Type de post" items={['Vente', 'Evénement', 'Poste à pourvoir', 'Promotion']} onCheckboxChange={handleCheckboxChange} />
+            <DropDownBtn text="Type de post" items={['Vente', 'Evénement', 'Poste à pourvoir', 'Promotion']} onCheckboxChange={handleCheckboxChange} />
         </div>
     );
 };
