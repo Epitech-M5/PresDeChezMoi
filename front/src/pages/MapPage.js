59a43354826cf7989d4e432095c1a798e86c2fd0
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-08-02 19:29:41 +0200

    map page done

--- front/src/pages/MapPage.js
+++ front/src/pages/MapPage.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { useSelector } from 'react-redux';
+import { getAPI } from '../api';
+import AddressDisplay from '../components/MainComponent/AddressDisplay';
+const adresseip = process.env.REACT_APP_BACKEND_ADRESSEIP
+const port = process.env.REACT_APP_BACKEND_PORT
 
 const MapPage = () => {
+
+    const user = useSelector((state) => state.utilisateur);
+
+    const [markers, setMarkers] = useState([]);
+    const [selectedPlace, setSelectedPlace] = useState(null);
+
+    useEffect(() => {
+        getAPI(`http://${adresseip}:${port}/api/annonce/`, {}, { 'x-access-token': user.token })
+            .then((response) => {
+
+                setTimeout(() => {
+                    setMarkers(response.dataAPI.map(item => ({
+                        ...item,
+                        latitude: item.latitude !== null ? Number(item.latitude) : null,
+                        longitude: item.longitude !== null ? Number(item.longitude) : null,
+                    })));
+                }, 4000)
+
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
+    }, [markers]);
 
     const mapStyles = {
         height: '100%',
@@ -9,11 +37,35 @@ const MapPage = () => {
     };
 
     const defaultCenter = {
-        lat: 48.8566, // ping paris
-        lng: 2.3522, //mettre les coordonnées de la ville
+        lat: 43.2965, // ping marseille default
+        lng: 5.3698, //mettre les coordonnées de la ville
 
     };
 
+    const handleMarkerClick = (place) => {
+        setSelectedPlace(place);
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    };
+
+    const renderDateCreate = (createdAt) => {
+        const formattedCreatedAt = formatDate(createdAt);
+
+        return (
+            <>
+                {formattedCreatedAt}
+            </>
+        );
+    };
+
+    const filteredMarkers = markers.filter((item) => item.latitude !== null && item.longitude !== null);
 
     return (
         <>
@@ -21,10 +73,26 @@ const MapPage = () => {
                 <div className="container_map_gg">
                     <LoadScript googleMapsApiKey="AIzaSyC5OdSI06hgsPRr1wzZgXMvDxBTep2-O7M">
                         <GoogleMap mapContainerStyle={mapStyles} zoom={10} center={defaultCenter}>
-                            <Marker position={defaultCenter} />
+
+                            {filteredMarkers.map((item) => (
+                                <Marker key={item.id} position={{ lat: item.latitude, lng: item.longitude }} onClick={() => handleMarkerClick(item)} />
+                            ))}
+
                         </GoogleMap>
                     </LoadScript>
                 </div>
+                {selectedPlace ? (
+                    <div className='container_info_marker_click'>
+                        <h1>Informations sur le lieu :</h1>
+                        <h2><span className='span_info_marker'>Adresse : </span><AddressDisplay longitude={selectedPlace.longitude} latitude={selectedPlace.latitude} /> le {renderDateCreate(selectedPlace.createdAt)}</h2>
+                        <h2><span className='span_info_marker'>Titre :</span> {selectedPlace.titre}</h2>
+                        {selectedPlace.description ? <h2><span className='span_info_marker'>Description :</span> {selectedPlace.description}</h2> : null}
+                    </div>
+                ) : (
+                    <div className='container_info_marker_click'>
+                        <h1>Sélectionnez un marqueur pour en savoir plus !</h1>
+                    </div>
+                )}
             </div>
         </>
     );
