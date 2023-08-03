2223fca1dea010fe2173936811194b210895efeb
Maxence Bonnici <maxence.bonnici@epitech.eu>
2023-08-03 16:07:26 +0200

    add env file, corr css

--- front/src/components/Admin/Post.js
+++ front/src/components/Admin/Post.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Post = () => {
+
+    const navigate = useNavigate();
+    const [activeId, setActiveId] = useState(null);
+
+    useEffect(() => {
+        setActiveId(1);
+    }, []);
+
+    const toggleUnderline = (id) => {
+        setActiveId(id);
+    }
+
+    const handleToggle = () => {
+
+        const toggleBtnIcon = document.querySelector('.container_forphone_responsive_admin i')
+        const ToSlide = document.querySelector('.navbar_toslide_responsive_admin')
+        ToSlide.classList.toggle('open')
+
+        const isOpen = ToSlide.classList.contains('open')
+        toggleBtnIcon.classList = isOpen ? 'fa-solid fa-greater-than fa-rotate-270' : 'fa-solid fa-greater-than fa-rotate-90'
+    }
+
     return (
         <>
             <div className='content_admin'>
-                <div className="container_title_page_admin">
-                    <h1>Filtrage annonces</h1>
-                </div>
                 <div className="content_inside_admin_pages">
-                    <h1>rentrer ce que vous voulez afficher ici, merci</h1>
+                    <div className="container_toggle_for_posts">
+
+                        <h1 className={`underline-animation ${activeId === 1 ? 'underline' : ''}`}
+                            onClick={() => toggleUnderline(1)}>Annonces signalées</h1>
+
+                        <h1 className={`underline-animation ${activeId === 2 ? 'underline' : ''}`}
+                            onClick={() => toggleUnderline(2)}>Annonces à valider</h1>
+
+                    </div>
                 </div>
             </div>
+
         </>
     );
 };
