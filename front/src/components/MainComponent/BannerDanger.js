72ed0bc0da233d5a80ea2eebe8d5c31a19ca8038
«boussaidm» <meriem.boussaid@epitech.eu>
2023-09-07 20:28:20 +0200

    design btn supp alerte def

--- front/src/components/MainComponent/BannerDanger.js
+++ front/src/components/MainComponent/BannerDanger.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { getAPI, postAPI, putAPI, deleteAPI } from "../../api.js";
-const adresseip = process.env.REACT_APP_BACKEND_ADRESSEIP
-const port = process.env.REACT_APP_BACKEND_PORT
+import { Provider, useDispatch, useSelector } from "react-redux";
+
+const adresseip = process.env.REACT_APP_BACKEND_ADRESSEIP;
+const port = process.env.REACT_APP_BACKEND_PORT;
 
 
 const DangerBanner = () => {
   const [showDangerBanner, setShowDangerBanner] = useState([]);
-
+  const user = useSelector((state) => state.utilisateur);
   useEffect(() => {
     const interval = setInterval(() => {
-
       // requête pour vérifier s'il y a une notification de type "DANGER" dans la base de données
       getAPI(`http://${adresseip}:${port}/api/notification`, null, {})
         .then((response) => {
-
-          setShowDangerBanner(response.dataAPI)
-
+          setShowDangerBanner(response.dataAPI);
         })
         .catch((error) => {
           console.log("error", error);
@@ -26,28 +25,49 @@ const DangerBanner = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const deleteAlerte = (idAlerte) => {
+    deleteAPI(
+      `http://${adresseip}:${port}/api/notification/${idAlerte}`,
+      null,
+      {}
+    )
+      .then((response) => {
+        console.log("Supp ok !");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   return (
-    <div className='contain_alert'>
+    <div className="contain_alert">
       {Array.isArray(showDangerBanner) &&
         showDangerBanner.map((banner) => (
-
-          <div key={banner.id} className='contain_message_banner'>
-
+          <div key={banner.id} className="contain_message_banner">
             {banner.formeNotif === "Bandereau" && (
               <>
+              {user.idRole === 3 && (
+               <div className="container_supp_alert ">
+               <button
+                 className="supp_alert"
+                 onClick={() => deleteAlerte(banner.id)}
+               >
+                 Supprimer l'alerte définitivement
+               </button>
+             </div>
+              )}
+ 
                 <div className="danger_banner_left">
                   <img src="media/img/danger_ban.png" alt="danger" />
                 </div>
+
                 <div className="danger_banner_right">
                   <h1>{banner.message}</h1>
                 </div>
               </>
             )}
-
           </div>
-        ))
-      }
+        ))}
     </div>
   );
 };
